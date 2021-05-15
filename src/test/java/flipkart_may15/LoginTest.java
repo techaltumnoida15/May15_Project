ee260d2455f4fd7f87c720af26aac9a1743955ac
Amit Kumar <techaltum.noida15@gmail.com>
2021-05-15 16:12:37 +0530

    Flipkart Login Test

---
+++ src/test/java/flipkart_may15/LoginTest.java
@@ -0,0 +1,12 @@
+package flipkart_may15;
+
+import org.projectName.com.BaseTest;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest{
+
+	@Test
+	public void loginTest() {
+		//Code
+	}
+}
