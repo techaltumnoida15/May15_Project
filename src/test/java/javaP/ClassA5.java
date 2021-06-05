3d879e77b0f9e9b6264e4fbb226710ef152e587a
Amit Kumar <techaltum.noida15@gmail.com>
2021-06-05 17:23:37 +0530

    Selenium Session - 05June

--- src/test/java/javaP/ClassA5.java
+++ src/test/java/javaP/ClassA5.java
@@ -3,6 +3,7 @@ package javaP;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ClassA5 {
 	
@@ -15,6 +16,7 @@ public class ClassA5 {
 		
 		int[] arr1 = {2, 1, 4, 6, 9, 7}; // Types to declare array in java?
 		
+		//steps
 		//sort this array - Assignment
 		
 		//get me a new array which should have value > 5
@@ -29,6 +31,24 @@ public class ClassA5 {
 		List<Integer> sortedList = list1.stream().sorted().collect(Collectors.toList());
 		System.out.println(sortedList);
 		
+		//int[] arr1 = {2, 4, 1, 6, 7, 9, 10};
+		
+		IntStream streamArray = Arrays.stream(arr1);
+		
+		streamArray.max();
+		
+		//Find Max num
+		//IntStream intStream = Arrays.stream(arr1);
+		//int maxItem = intStream.max().getAsInt();
+		//System.out.println("Max item in array = " + intStream.max());
+		//System.out.println("Max item in array = " + maxItem);
+		
+		//IntStream intStream = Arrays.stream(arr1);
+		
+		//IntStream.
+		
+		//Code
+		
 		/*
 		 * Open flipkart
 		 * 
@@ -36,7 +56,7 @@ public class ClassA5 {
 		 * 
 		 * Select option "Sort by Price"
 		 * 
-		 * Get price of mobiles, and check if they are sorted by price or not
+		 * Get price of mobiles/items, and check if they are sorted by price or not
 		 * 
 		 */
 		
--- src/test/java/snapdeal_2305/SnapDeal.java
+++ src/test/java/snapdeal_2305/SnapDeal.java
@@ -16,66 +16,66 @@ import org.testng.annotations.Test;
 
 @Test
 public class SnapDeal extends BaseTest {
-	
-	
+
+
 	public void SdLogin() throws InterruptedException {
-		
-//Assignment 1 - Go to snapdeal.com
-		
-	driver.navigate().to("https://www.snapdeal.com/");
-	
-	
-	
-	// Assignment 2- Find the sign- in locator 
 
-	WebElement signIn = driver.findElement(By.xpath("//span[@class='accountUserName col-xs-12 reset-padding']"));
-	
-	//Assignment 3 -Mouse over on the Sign-in element
-	
-	Actions objActions = new Actions (driver);
-	
-	objActions.moveToElement(signIn);
-	
-	// Build function
-	
-	objActions.build().perform();
-	
-	Thread.sleep(4000);
-	
-	System.out.println("Sign-in button hovered");
-	
-	// Assignment 4- Click on Login
-	
-	WebElement loginElement  = driver.findElement(By.xpath("//a[text()='login']"));
-	loginElement.click();
-	
-	System.out.println("Sign-in clicked");
-	
-	//Assignment 5- Enter mobile number on the login popup\
-	
+		//Assignment 1 - Go to snapdeal.com
 
-	//WebElement Mobileinputbox = driver.findElement(By.xpath("//*[@id=\"userName\"]"));
-	
-	WebElement Mobileinputbox = driver.findElement(By.xpath("//input[@name='username']"));
-	
-	driver.switchTo().frame(Mobileinputbox);
-	
-	System.out.println("Popup Frame selected to Enter mobilEnter number");
-	
+		driver.navigate().to("https://www.snapdeal.com/");
 
+
+
+		// Assignment 2- Find the sign- in locator 
+
+		WebElement signIn = driver.findElement(By.xpath("//span[@class='accountUserName col-xs-12 reset-padding']"));
+
+		//Assignment 3 -Mouse over on the Sign-in element
+
+		Actions objActions = new Actions (driver);
+
+		objActions.moveToElement(signIn);
+
+		// Build function
+
+		objActions.build().perform();
+
+		Thread.sleep(4000);
+
+		System.out.println("Sign-in button hovered");
+
+		// Assignment 4- Click on Login
+
+		WebElement loginElement  = driver.findElement(By.xpath("//a[text()='login']"));
+		loginElement.click();
+
+		System.out.println("Sign-in clicked");
+
+		//Assignment 5- Enter mobile number on the login popup\
+
+
+		//WebElement Mobileinputbox = driver.findElement(By.xpath("//*[@id=\"userName\"]"));
+
+		//driver.switchTo().frame(Mobileinputbox);
+		WebElement frameLogin = driver.findElement(By.id("loginIframe"));
+		driver.switchTo().frame(frameLogin);
+
+		System.out.println("Popup Frame selected to Enter mobilEnter number");
+
+		WebElement Mobileinputbox = driver.findElement(By.xpath("//input[@name='username']"));
 		Mobileinputbox.sendKeys("8447855711");
-		
-		//Click Continue
-		
-		WebElement continueBT = driver.findElement(By.xpath("//*[@id='checkUser']"));
-		
-		continueBT.click();
-		
-		
-	}
-	
 
-	
+		//Click Continue
+
+		WebElement continueBT = driver.findElement(By.xpath("//*[@id='checkUser']"));
+
+		continueBT.click();
+
+
 	}
+
+
+
+}
 
 
